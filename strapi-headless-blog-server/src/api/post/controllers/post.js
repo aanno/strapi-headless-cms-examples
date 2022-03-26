4e2d826a8d8d7a43c7a4a3dee76219a8909abccf
aanno <aanno@users.noreply.github.com>
2022-03-26 19:28:20 +0100

    post js fixes

--- strapi-headless-blog-server/src/api/post/controllers/post.js
+++ strapi-headless-blog-server/src/api/post/controllers/post.js
@@ -8,36 +8,44 @@ const { createCoreController } = require('@strapi/strapi').factories;
 const readingTime = require('reading-time');
 
 module.exports = createCoreController('api::post.post', ({ strapi }) => ({
-            async create(ctx) {
-                // some logic here
-                const { content } = ctx;
-                if (content && content.length && content.length > 0) {
-                    const ct = readingTime(content);
-                    if (ct) {
-                        data.readingTime = ct.text || undefined;
-                    }
-                }
-
-                const response = await super.create(ctx);
-
-                // some more logic
-
-                return response;
-            },
-            async update(ctx) {
-                // some logic here
-                const { content } = ctx;
-                if (content && content.length && content.length > 0) {
-                    const ct = readingTime(content);
-                    if (ct) {
-                        data.readingTime = ct.text || undefined;
-                    }
-                }
-
-                const response = await super.update(ctx);
-
-                // some more logic
-
-                return response;
+    async create(ctx) {
+        // some logic here
+        var readingTimeMin;
+        const { content } = ctx;
+        if (content && content.length && content.length > 0) {
+            const ct = readingTime(content);
+            if (ct) {
+                readingTimeMin = ct.text || undefined;
             }
-        }));
+        }
+
+        const response = await super.create(ctx);
+
+        // some more logic
+        if (readingTimeMin) {
+            response.readingTimeMin = readingTimeMin;
+        }
+
+        return response;
+    },
+    async update(ctx) {
+        // some logic here
+        var readingTimeMin;
+        const { content } = ctx;
+        if (content && content.length && content.length > 0) {
+            const ct = readingTime(content);
+            if (ct) {
+                readingTimeMin = ct.text || undefined;
+            }
+        }
+
+        const response = await super.update(ctx);
+
+        // some more logic
+        if (readingTimeMin) {
+            response.readingTimeMin = readingTimeMin;
+        }
+
+        return response;
+    }
+}));
