4fbb0594a79be3b96c2a309b1618a985b99cff0c
aanno <aanno@users.noreply.github.com>
2022-03-26 20:30:29 +0100

    client added tailwindcss

---
+++ client-astro-react-tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
