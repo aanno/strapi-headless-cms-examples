818498e5bd76cda513db851f6b46d0e6340a5c7e
aanno <aanno@users.noreply.github.com>
2022-03-26 20:54:36 +0100

    fixes

--- client-astro-react-tailwind/astro.config.mjs
+++ client-astro-react-tailwind/astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import astro_react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
     devOptions: {
         tailwindConfig: './tailwind.config.js',
     },
+    integrations: [
+        astro_react(),
+    ],
 });
